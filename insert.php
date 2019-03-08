50fa1ec411ffca29fef5af01d1481ffe2ed727b0
Giovanni Rizza <rizza67jorge@gmail.com>
2019-03-08 12:29:44 +0100

    Nubi Interaction tbf

--- insert.php
+++ insert.php
@@ -22,9 +22,28 @@
         <!-- Main styles for this application-->
         <link href="css/style.min.css" rel="stylesheet">
         <link href="css/kstyle.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <!-- Global site tag (gtag.js) - Google Analytics-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script>
+          $(document).ready(function(){
+            $('#hid').hide();
 
+            $('.add').on('click', function(){
+              if ($('#ic').hasClass('fa fa-plus-circle')) {
+                $('#ic').removeClass('fa fa-plus-circle');
+                $('#ic').addClass('fa fa-minus-circle');
+              }
+              else {
+                $('#ic').removeClass('fa fa-minus-circle');
+                $('#ic').addClass('fa fa-plus-circle');
+              }
+              $('#hid').slideToggle();
+
+            })
+
+          })
+        </script>
       </head>
       <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
         <header class="app-header navbar">
@@ -56,15 +75,17 @@
                             <td>Data e Ora</td>
                             <td><input class="form-control" value="<?php echo $t; ?>" name="wxTime"></td>
                           </tr>
+
                           <tr>
                             <td>Direzione Vento</td>
                             <td><input class="form-control" name="wxDirV"></td>
                           </tr>
+
                           <tr>
                             <td>Vel. Vento</td>
                             <td><input class="form-control" name="wxVelV"></td>
-
                           </tr>
+
                           <tr>
                             <td>Visibilita</td>
                             <td><input class="form-control" name="wxVis" placeholder="<CAVOK>"></td>
@@ -73,40 +94,69 @@
                           <tr>
                             <td>Fenomeni</td>
                             <td><input class="form-control" name="wxFen"></td>
+                          </tr>
 
-                          </tr>
                           <tr>
-                            <td>Cop. Nuvolosa</td>
+                            <td>Cop. Nuvolosa<br />
+                            <div class="add">
+                              <i>Tipo Nubi</i>
+                              <i id="ic" class="fa fa-plus-circle"></i>
+                            </div>
+                            </td>
                             <td><input class="form-control" name="wxNuv">
+                              <div id="hid">
+                                <select class="form-control" name="wx1stNuv">
+                                  <option value="">Non specificata</option>
+                                  <?php
+                                    $sql = "SELECT id, nube FROM cldtypes";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_assoc($result)){
+                                      echo '<option value="'.$row['id'].'">'.$row['nube'].'</option>';
+                                    }
+                                  ?>
+                                </select>
+                                <select class="form-control" name="wx2ndNuv">
+                                  <option value="">Non specificata</option>
+                                  <?php
+                                    $sql = "SELECT id, nube FROM cldtypes";
+                                    $result = mysqli_query($conn, $sql);
+                                    while ($row = mysqli_fetch_assoc($result)){
+                                      echo '<option value="'.$row['id'].'">'.$row['nube'].'</option>';
+                                    }
+                                  ?>
+                                </select>
+                              </div>
+
+                            </td>
                           </tr>
+
                           <tr>
                             <td>Temperatura</td>
                             <td><input type="number" class="form-control" name="wxTA"></td>
-
                           </tr>
+
                           <tr>
                             <td>P. di rugiada</td>
                             <td><input type="number" class="form-control" name="wxTR"></td>
-
                           </tr>
+
                           <tr>
                             <td>Pressione</td>
                             <td><input class="form-control" name="wxPress"></td>
-
                           </tr>
+
                           <tr>
                             <td>Trend</td>
                             <td><input class="form-control" name="wxTrend"></td>
-
                           </tr>
+
                           <tr>
                             <td>Note</td>
                             <td><input class="form-control" name="wxRmk"></td>
-
                           </tr>
-                          <tr>
-                            <td><input type="submit" name="wxNew"></td>
 
+                          <tr>
+                            <td colspan="2"><input type="submit" name="wxNew"></td>
                           </tr>
                           </form>
                         </tbody>
